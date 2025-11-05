854589005be03151347efc6a65f08445969ee749
AlexRen998916 <rentianqi3@huawei.com>
2025-11-05 16:57:43 +0800

    get device_ip from hccn_tool when /etc/hccn.conf does not exist

--- tests/depends/llm_datadist/src/data_cache_engine_test_helper.h
+++ tests/depends/llm_datadist/src/data_cache_engine_test_helper.h
@@ -54,6 +54,30 @@ class HcclApiStub {
   static std::unique_ptr<HcclApiStub> instance_;
 };
 
+class MockMmpaForHccnTool : public llm::MmpaStubApiGe {
+  public:
+  static void Install() {
+    llm::MmpaStub::GetInstance().SetImpl(std::make_shared<MockMmpaForHccnTool>());
+  }
+  static void Reset() {
+    HcclApiStub::ResetStub();
+    llm::MmpaStub::GetInstance().Reset();
+  }
+
+  void *DlOpen(const char *file_name, int32_t mode) override;
+
+  int32_t DlClose(void *handle) override;
+
+  int32_t RealPath(const CHAR *path, CHAR *realPath, INT32 realPathLen) override {
+    std::string stub_path = path;
+    if (stub_path == "/etc/hccn.conf") {
+      stub_path = "/tmp/hccn.conf";
+    }
+    memcpy_s(realPath, realPathLen, stub_path.c_str(), stub_path.length());
+    return 0;
+  }
+};
+
 class MockMmpaForHcclApi : public llm::MmpaStubApiGe {
  public:
   static void Install() {
@@ -71,12 +95,7 @@ class MockMmpaForHcclApi : public llm::MmpaStubApiGe {
   int32_t DlClose(void *handle) override;
 
   int32_t RealPath(const CHAR *path, CHAR *realPath, INT32 realPathLen) override {
-    std::string stub_path = path;
-    if (stub_path == "/etc/hccn.conf") {
-      stub_path = "/tmp/hccn.conf";
-    }
-    memcpy_s(realPath, realPathLen, stub_path.c_str(), stub_path.length());
-    return 0;
+    return 1;
   }
 
  private:
@@ -152,6 +171,22 @@ class AutoCommResRuntimeMock : public llm::RuntimeStub {
   static void Reset() {
     llm::RuntimeStub::Reset();
     RemoveHccnConfFile();
+  }
+
+  static void InstallWithoutHccnConfFile() {
+    llm::RuntimeStub::SetInstance(std::make_shared<AutoCommResRuntimeMock>());
+  }
+
+  static void ResetWithoutHccnConfFile() {
+    llm::RuntimeStub::Reset();
+  }
+
+  static void DeleteHccnConfIfExist() {
+    std::string path = "/tmp/hccn.conf";
+    if (FILE *file = fopen(path.c_str(), "r")) {
+      fclose(file);
+      std::remove(path.c_str());
+    }
   }
 
   rtError_t rtGetSocVersion(char *version, const uint32_t maxLen) override {
