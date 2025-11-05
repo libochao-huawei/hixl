a822fe5282dcb285ac1326740e8ea410ee3e909b
AlexRen998916 <rentianqi3@huawei.com>
2025-11-05 16:57:43 +0800

    get device_ip from hccn_tool when /etc/hccn.conf does not exist

--- tests/depends/llm_datadist/src/data_cache_engine_test_helper.h
+++ tests/depends/llm_datadist/src/data_cache_engine_test_helper.h
@@ -76,8 +76,18 @@ class MockMmpaForHcclApi : public llm::MmpaStubApiGe {
       stub_path = "/tmp/hccn.conf";
     }
     memcpy_s(realPath, realPathLen, stub_path.c_str(), stub_path.length());
-    return 0;
+    std::string tempPath = "/tmp/hccn.conf";
+    if (FILE *file = fopen(tempPath.c_str(), "r")) {
+      fclose(file);
+      return 0;
+    } else {
+      return 1;
+    }
   }
+
+  std::string HccnOutPut(std::string command) override {
+    return "ipaddr:127.0.0.1";
+  } 
 
  private:
   static HcclResult HcclExchangeMemDesc(HcclComm comm, uint32_t remoteRank, HcclMemDescs *local, int timeout,
@@ -152,6 +162,22 @@ class AutoCommResRuntimeMock : public llm::RuntimeStub {
   static void Reset() {
     llm::RuntimeStub::Reset();
     RemoveHccnConfFile();
+  }
+
+  static void InstallWithoutHccnConfFile() {
+    llm::RuntimeStub::SetInstance(std::make_shared<AutoCommResRuntimeMock>());
+  }
+
+  static void ResetWithoutHccnConfFile() {
+    llm::RuntimeStub::Reset();
+  }
+
+  static void DeleteHccnConfIfExist() {
+    std::string path = "/tmp/hccn.conf";
+    if (FILE *file = fopen(path.c_str(), "r")) {
+      fclose(file);
+      std::remove(path.c_str());
+    }
   }
 
   rtError_t rtGetSocVersion(char *version, const uint32_t maxLen) override {
