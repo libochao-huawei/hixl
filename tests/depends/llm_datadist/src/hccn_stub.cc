cc344ada2d8af786faa61f868723f86dd47e11ad
AlexRen998916 <rentianqi3@huawei.com>
2025-11-05 16:57:43 +0800

    get device_ip from hccn_tool when /etc/hccn.conf does not exist

---
+++ tests/depends/llm_datadist/src/hccn_stub.cc
@@ -0,0 +1,16 @@
+#include <string>
+
+extern "C" {
+    // std::string GetIpAddressFromHccnTool(uint32_t phy_device_id) {
+    //     std::string command = "";
+    //     return GetHccnOutput(command);
+    // }
+
+    std::string LocalCommResGenerator::GetHccnOutput(std::string command) {
+        return "ipaddr:127.0.0.1";
+    }
+
+    int32_t mmRealPath(const char *path, char *realPath, int32_t realPathLen) {
+        return 1;
+    }
+}
