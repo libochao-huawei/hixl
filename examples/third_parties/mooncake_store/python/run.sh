53fd3841379ad32f28e0c35d43d3dcec19f18cc4
ZhaoBaiwei <zhaobaiwei@huawei.com>
2025-11-11 17:23:01 +0800

    修改run.sh移除无用配置

--- examples/third_parties/mooncake_store/python/run.sh
+++ examples/third_parties/mooncake_store/python/run.sh
@@ -14,13 +14,12 @@ export ASCEND_GLOBAL_EVENT_ENABLE=1
 export ASCEND_HOST_LOG_FILE_NUM=500
 # export HCCL_INTRA_PCIE_ENABLE=1
 
-# 默认走hccs，可以通过下面参数调整为roce
+# 机器内默认走hccs，可以通过下面参数调整为roce
 # export HCCL_INTRA_ROCE_ENABLE=1
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
 # mooncake参数
 export ASCEND_BUFFER_POOL=4:8 # adxl环境变量 BUFFER_NUM:BUFFER_SIZE (MB)
-export MC_ALLOC_SAME_NODE_FIRST=1 # 内存分配策略，优先在同一个NUMA节点上分配
 
 python3 $@
