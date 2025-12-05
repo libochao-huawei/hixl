e96d8faa28fc4b0aa17c758bf53384eb310982ed
youxiao <youxiao@huawei.com>
2025-12-05 17:16:34 +0800

    add lock for channel finalize and transfer

--- src/llm_datadist/adxl/channel_msg_handler.cc
+++ src/llm_datadist/adxl/channel_msg_handler.cc
@@ -334,8 +334,8 @@ Status ChannelMsgHandler::ProcessConnectRequest(int32_t fd, const std::vector<ch
     ret = ConnectInfoProcess(peer_connect_info, peer_connect_info.timeout, false);
   }
   if (ret == SUCCESS) {
-    LLMLOGI("Success to process connect info, local engine:%s, remote engine:%s, timeout:%d ms.",
-           listen_info_.c_str(), peer_connect_info.channel_id.c_str(), peer_connect_info.timeout);
+    LLMEVENT("Success to process connect info, local engine:%s, remote engine:%s, timeout:%d ms.", listen_info_.c_str(),
+             peer_connect_info.channel_id.c_str(), peer_connect_info.timeout);
   }
   ADXL_CHK_STATUS(ret, "Failed to process connect info, local engine:%s, remote engine:%s, timeout:%d ms.",
                   listen_info_.c_str(), peer_connect_info.channel_id.c_str(), peer_connect_info.timeout);
@@ -475,7 +475,6 @@ Status ChannelMsgHandler::Disconnect(const std::string &remote_engine, int32_t t
   auto channel = channel_manager_->GetChannel(ChannelType::kClient, remote_engine);
   ADXL_CHK_BOOL_RET_STATUS(channel != nullptr, NOT_CONNECTED,
                            "Failed to get channel, channel_id:%s", remote_engine.c_str());
-  std::lock_guard<std::mutex> transfer_lock(channel->GetTransferMutex());
   channel->StopHeartbeat();
   // if connect failed, then release client and server auto release channel
   ADXL_CHK_STATUS(llm::MsgHandlerPlugin::Connect(remote_ip, static_cast<uint32_t>(remote_port),
