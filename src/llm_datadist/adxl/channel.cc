e5fe241239ed062c01770cfcab63514f7034cfda
zhangcheng <zhangcheng299@huawei.com>
2025-12-08 16:05:15 +0800

    修复d2rh场景下任务传输失败，event查询状态成功的问题

--- src/llm_datadist/adxl/channel.cc
+++ src/llm_datadist/adxl/channel.cc
@@ -168,6 +168,13 @@ Status Channel::GetTransferStatus(const TransferReq &req, TransferStatus &status
     status = TransferStatus::WAITING;
     return SUCCESS;
   }
+  auto steam_status = rtStreamSynchronize(stream_);
+  if (steam_status != RT_ERROR_NONE) {
+    status = TransferStatus::FAILED;
+    rtEventDestroy(event);
+    LLMLOGE(FAILED, "rtStreamSyncronize failed for req:%llu, ret:%d.", id, steam_status);
+    return FAILED;
+  }
   LLMLOGI("Transfer async request completed, req:%llu.", id);
   status = TransferStatus::COMPLETED;
   rtEventDestroy(event);
