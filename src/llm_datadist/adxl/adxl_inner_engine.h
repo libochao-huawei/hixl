f30e8c41c83fa787ad148f99181454068c290825
zhangcheng <zhangcheng299@huawei.com>
2026-02-03 11:15:57 +0800

    [feat] 支持client侧自动清理异常链路资源&&支持自动建链模式

--- src/llm_datadist/adxl/adxl_inner_engine.h
+++ src/llm_datadist/adxl/adxl_inner_engine.h
@@ -76,6 +76,8 @@ class AdxlInnerEngine {
   Status ParseBufferPoolParams(const std::map<AscendString, AscendString> &options, uint64_t &buffer_size,
                                uint64_t &npu_pool_size);
   Status ParseEnableFabricMem(const std::map<AscendString, AscendString> &options);
+  Status ParseAutoConnectConfig(const std::map<AscendString, AscendString> &options);
+  Status DisconnectOnError(const std::string &remote_engine, int32_t timeout_in_millis);
 
   std::string local_engine_;
   ChannelManager channel_manager_;
@@ -90,6 +92,7 @@ class AdxlInnerEngine {
   std::unique_ptr<StreamPool> stream_pool_ = nullptr;
   bool user_config_buffer_pool_{false};
   bool user_config_channel_pool_{false};
+  bool auto_connect_{false};
   aclrtContext aclrt_context_{nullptr};
 
   std::mutex notify_mutex_;
