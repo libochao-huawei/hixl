d7c1577bf2bd20722d2e735dcdfe31333fa22cb5
zhaozhihui <zhaozhihui3@yeah.net>
2026-02-11 16:56:14 +0800

    fix

--- examples/python/hixl_tranfer_backend_sample.py
+++ examples/python/hixl_tranfer_backend_sample.py
@@ -84,14 +84,15 @@ def run_prompt_sample(datadist, local_host_ip, remote_host_ip):
     # 等对端推送完成
     dist.barrier()
 
-    logging.info(f'after decoder push, tensor={tensor.cpu()}')
-    logging.info(f'after decoder push, tensor2={tensor2.cpu()}')
+    logging.info(f'after push and pull, tensor={tensor.cpu()}')
+    logging.info(f'after push and pull, tensor2={tensor2.cpu()}')
 
     expect_tensor = torch.full((BLOCKS_NUM, KV_SHAPE), 0, dtype=torch.float).npu()
     if not torch.equal(expect_tensor, tensor):
         raise Exception(f"check tensor val failed, tensor={tensor.cpu()}, expect_tensor={expect_tensor.cpu()}")
     if not torch.equal(expect_tensor, tensor2):
         raise Exception(f"check tensor val failed, tensor2={tensor2.cpu()}, expect_tensor={expect_tensor.cpu()}")
+    logging.info('check tensor val success')
 
     # 断链
     cluster = LLMClusterInfo()
