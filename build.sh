ebe8fcffbc0e85f36f59b984f67bd1d68010111e
chenkunjie0506 <chenkunjie1@huawei.com>
2026-02-24 09:11:37 +0800

    fix comment

--- build.sh
+++ build.sh
@@ -67,8 +67,6 @@ checkopts() {
   ENABLE_ASAN=OFF
   ENABLE_GCOV=OFF
   ENABLE_SIGN=OFF
-  #CUSTOM_SIGN_SCRIPT="${BASEPATH}/scripts/sign/community_sign_build.py"
-  # 黄区的签名脚本
   CUSTOM_SIGN_SCRIPT="${BASEPATH}/../vendor/hisi/build/scripts/sign_and_add_header.sh"
   VERSION_INFO="8.5.0"
 
@@ -82,7 +80,7 @@ checkopts() {
   fi
 
   # Process the options
-  parsed_args=$(getopt -a -o j:hv -l help,verbose,pkg,examples,cann_3rd_lib_path:,cann-3rd-lib-path:,output_path:,output-path:,build_type:,build-type:,sign-script:,sign_script:,asan,cov,enable_sign,enable-sign -- "$@") || {
+  parsed_args=$(getopt -a -o j:hv -l help,verbose,pkg,examples,cann_3rd_lib_path:,cann-3rd-lib-path:,output_path:,output-path:,build_type:,build-type:,sign-script:,sign_script:,asan,cov,enable_sign,enable-sign,version -- "$@") || {
     usage
     exit 1
   }
@@ -143,6 +141,10 @@ checkopts() {
         shift
         break
         ;;
+      --version)
+        VERSION_INFO="$2"
+        shift 2
+        ;;
       --cov)
         ENABLE_GCOV=ON
         CMAKE_BUILD_TYPE="Debug"
@@ -180,7 +182,6 @@ build() {
         -D ENABLE_GCOV=${ENABLE_GCOV} \
         -D ENABLE_SIGN=${ENABLE_SIGN} \
         -D CUSTOM_SIGN_SCRIPT=${CUSTOM_SIGN_SCRIPT} \
-        -D VERSION_INFO=${VERSION_INFO} \
         -D AARCH_PREFIX=${AARCH_PREFIX} \
         ${CANN_3RD_LIB_PATH:+-D CANN_3RD_LIB_PATH=${CANN_3RD_LIB_PATH}} \
         ..
