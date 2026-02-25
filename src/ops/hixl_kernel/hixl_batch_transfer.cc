f01b311216d96dff3517f36cc59186ea3e2fa406
lugang11 <lugang11@huawei.com>
2026-02-25 20:18:19 +0800

    覆盖率整改

--- src/ops/hixl_kernel/hixl_batch_transfer.cc
+++ src/ops/hixl_kernel/hixl_batch_transfer.cc
@@ -10,6 +10,7 @@
 #include "hixl_batch_transfer.h"
 #include <string>
 #include "common/hixl_log.h"
+#include "common/hixl_checker.h"
 #include "hixl/hixl.h"
 
 namespace {
@@ -20,10 +21,10 @@ uint32_t HixlBatchTransferTask(bool is_read, HixlOneSideOpParam *param) {
       HIXL_LOGI(
           "[HixlBatchPutAndGet] HcommReadOnThread start, list_num=%u, i=%u, thread=%u, channel=%u, "
           "dst_buf_list[%u]=%p, src_buf_list[%u]=%p, len_list[%u]=%lu",
-          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i, param->src_buf_addr_list[i], i,
-          param->len_list[i]);
-      int32_t ret = HcommReadOnThread(param->thread, param->channel, param->dst_buf_addr_list[i], param->src_buf_addr_list[i],
-                                      param->len_list[i]);
+          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i,
+          param->src_buf_addr_list[i], i, param->len_list[i]);
+      int32_t ret = HcommReadOnThread(param->thread, param->channel, param->dst_buf_addr_list[i],
+                                      param->src_buf_addr_list[i], param->len_list[i]);
       if (ret != 0) {
         HIXL_LOGE(hixl::FAILED,
                   "HcommReadOnThread failed. The address information is as follows:dst_buf:%p, scr_buf:%p, buf_len:%u, "
@@ -38,10 +39,10 @@ uint32_t HixlBatchTransferTask(bool is_read, HixlOneSideOpParam *param) {
       HIXL_LOGI(
           "[HixlBatchPutAndGet] HcommReadOnThread start, list_num=%u, i=%u, thread=%u, channel=%u, "
           "dst_buf_list[%u]=%p, src_buf_list[%u]=%p, len_list[%u]=%lu",
-          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i, param->src_buf_addr_list[i], i,
-          param->len_list[i]);
-      int32_t ret = HcommWriteOnThread(param->thread, param->channel, param->dst_buf_addr_list[i], param->src_buf_addr_list[i],
-                                       param->len_list[i]);
+          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i,
+          param->src_buf_addr_list[i], i, param->len_list[i]);
+      int32_t ret = HcommWriteOnThread(param->thread, param->channel, param->dst_buf_addr_list[i],
+                                       param->src_buf_addr_list[i], param->len_list[i]);
       if (ret != 0) {
         HIXL_LOGE(hixl::FAILED,
                   "HcommWriteOnThread failed. The address information is as follows:dst_buf:%p, scr_buf:%p, "
@@ -64,40 +65,27 @@ uint32_t HixlBatchTransfer(bool is_read, HixlOneSideOpParam *param) {
   constexpr const char *kBatchTag = "HixlKernel";
   int32_t ret = HcommBatchModeStart(kBatchTag);
   HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeStart end");
-  if (ret != 0) {
-    HIXL_LOGE(hixl::FAILED, "[HixlBatchPutAndGet] HcommBatchModeStart faild, ret is %d", ret);
-    return hixl::FAILED;
-  }
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, hixl::FAILED, "[HixlBatchPutAndGet] HcommBatchModeStart faild, ret is %d", ret);
   ret = HixlBatchTransferTask(is_read, param);
-  if (ret != 0) {
-    HIXL_LOGE(hixl::FAILED, "[HixlBatchPutAndGet] HixlBatchTransferTask faild, ret is %d", ret);
-    return hixl::FAILED;
-  }
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, hixl::FAILED, "[HixlBatchPutAndGet] HixlBatchTransferTask faild, ret is %d", ret);
   ret = HcommChannelFenceOnThread(param->thread, param->channel);
-  if (ret != 0) {
-    HIXL_LOGE(hixl::FAILED, "[HixlBatchPutAndGet] HcommChannelFenceOnThread faild, ret is %d", ret);
-    return hixl::FAILED;
-  }
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, hixl::FAILED, "[HixlBatchPutAndGet] HcommChannelFenceOnThread faild, ret is %d",
+                           ret);
   HIXL_LOGI(
       "[HixlBatchPutAndGet] HcommReadOnThread start to read remote flag, param->flag_size=%u, param->local_flag=%lu, "
       "param->remote_flag=%lu",
       param->flag_size, param->local_flag_addr, param->remote_flag_addr);
-  ret = HcommReadOnThread(param->thread, param->channel,
-                          reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(param->local_flag_addr)),
-                          reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(param->remote_flag_addr)), param->flag_size);
-  if (ret != 0) {
-    HIXL_LOGE(hixl::FAILED,
-              "[HixlBatchPutAndGet] Remote flag read failed. The address information is as follows:dst_buf:%u, "
-              "scr_buf:%u, buf_len:%u, ret is %d.",
-              param->local_flag_addr, param->remote_flag_addr, param->flag_size, ret);
-    return hixl::FAILED;
-  }
+  ret = HcommReadOnThread(
+      param->thread, param->channel, reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(param->local_flag_addr)),
+      reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(param->remote_flag_addr)), param->flag_size);
+  HIXL_CHK_BOOL_RET_STATUS(
+      ret == 0, hixl::FAILED,
+      "[HixlBatchPutAndGet] Remote flag read failed. The address information is as follows:dst_buf:%lu, "
+      "scr_buf:%lu, buf_len:%u, ret is %d.",
+      param->local_flag_addr, param->remote_flag_addr, param->flag_size, ret);
   HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeEnd start");
   ret = HcommBatchModeEnd(kBatchTag);
-  if (ret != 0) {
-    HIXL_LOGE(hixl::FAILED, "[HixlBatchPutAndGet] HcommBatchModeEnd faild, ret is %d", ret);
-    return hixl::FAILED;
-  }
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, hixl::FAILED, "[HixlBatchPutAndGet] HcommBatchModeEnd faild, ret is %d", ret);
   HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeEnd end");
   return hixl::SUCCESS;
 }
