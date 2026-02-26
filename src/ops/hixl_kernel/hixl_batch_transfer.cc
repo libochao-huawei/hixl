d58735b003bcb3b2be158528ed7c261d0ba3c12b
lugang11 <lugang11@huawei.com>
2026-02-26 12:35:43 +0800

    [feat]单边通信新增算子包

---
+++ src/ops/hixl_kernel/hixl_batch_transfer.cc
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#include "hixl_batch_transfer.h"
+#include <string>
+#include "common/hixl_log.h"
+#include "common/hixl_checker.h"
+#include "hixl/hixl.h"
+
+namespace hixl {
+namespace {
+uint32_t HixlBatchTransferTask(bool is_read, HixlOneSideOpParam *param) {
+  if (is_read) {
+    // 批量提交读任务
+    for (uint32_t i = 0; i < param->list_num; i++) {
+      HIXL_LOGI(
+          "[HixlBatchPutAndGet] HcommReadOnThread start, list_num=%u, i=%u, thread=%u, channel=%u, "
+          "dst_buf_list[%u]=%p, src_buf_list[%u]=%p, len_list[%u]=%lu",
+          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i,
+          param->src_buf_addr_list[i], i, param->len_list[i]);
+      int32_t ret = HcommReadOnThread(param->thread, param->channel, param->dst_buf_addr_list[i],
+                                      param->src_buf_addr_list[i], param->len_list[i]);
+      if (ret != 0) {
+        HIXL_LOGE(FAILED,
+                  "HcommReadOnThread failed. The address information is as follows:dst_buf:%p, scr_buf:%p, buf_len:%u, "
+                  "ret is %d.",
+                  param->dst_buf_addr_list[i], param->src_buf_addr_list[i], param->len_list[i], ret);
+        return FAILED;
+      }
+    }
+  } else {
+    // 批量提交写任务
+    for (uint32_t i = 0; i < param->list_num; i++) {
+      HIXL_LOGI(
+          "[HixlBatchPutAndGet] HcommReadOnThread start, list_num=%u, i=%u, thread=%u, channel=%u, "
+          "dst_buf_list[%u]=%p, src_buf_list[%u]=%p, len_list[%u]=%lu",
+          param->list_num, i, param->thread, param->channel, i, param->dst_buf_addr_list[i], i,
+          param->src_buf_addr_list[i], i, param->len_list[i]);
+      int32_t ret = HcommWriteOnThread(param->thread, param->channel, param->dst_buf_addr_list[i],
+                                       param->src_buf_addr_list[i], param->len_list[i]);
+      if (ret != 0) {
+        HIXL_LOGE(FAILED,
+                  "HcommWriteOnThread failed. The address information is as follows:dst_buf:%p, scr_buf:%p, "
+                  "buf_len:%u, ret is %d.",
+                  param->dst_buf_addr_list[i], param->src_buf_addr_list[i], param->len_list[i], ret);
+        return FAILED;
+      }
+    }
+  }
+  return SUCCESS;
+}
+
+uint32_t HixlBatchTransfer(bool is_read, HixlOneSideOpParam *param) {
+  HIXL_LOGI("[HixlBatchPutAndGet] HixlBatchTransfer %s start.", is_read ? "read" : "write");
+  if (param == nullptr) {
+    HIXL_LOGE(PARAM_INVALID, "[HixlBatchPutAndGet] param is nullptr");
+    return PARAM_INVALID;
+  }
+  HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeStart start");
+  constexpr const char *kBatchTag = "HixlKernel";
+  int32_t ret = HcommBatchModeStart(kBatchTag);
+  HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeStart end");
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, FAILED, "[HixlBatchPutAndGet] HcommBatchModeStart faild, ret is %d", ret);
+  ret = HixlBatchTransferTask(is_read, param);
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, FAILED, "[HixlBatchPutAndGet] HixlBatchTransferTask faild, ret is %d", ret);
+  ret = HcommChannelFenceOnThread(param->thread, param->channel);
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, FAILED, "[HixlBatchPutAndGet] HcommChannelFenceOnThread faild, ret is %d",
+                           ret);
+  HIXL_LOGI(
+      "[HixlBatchPutAndGet] HcommReadOnThread start to read remote flag, param->flag_size=%u, param->local_flag=%lu, "
+      "param->remote_flag=%lu",
+      param->flag_size, param->local_flag_addr, param->remote_flag_addr);
+  ret = HcommReadOnThread(param->thread, param->channel,
+                          reinterpret_cast<void *>(static_cast<uintptr_t>(param->local_flag_addr)),
+                          reinterpret_cast<void *>(static_cast<uintptr_t>(param->remote_flag_addr)), param->flag_size);
+  HIXL_CHK_BOOL_RET_STATUS(
+      ret == 0, FAILED,
+      "[HixlBatchPutAndGet] Remote flag read failed. The address information is as follows:dst_buf:%lu, "
+      "scr_buf:%lu, buf_len:%u, ret is %d.",
+      param->local_flag_addr, param->remote_flag_addr, param->flag_size, ret);
+  HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeEnd start");
+  ret = HcommBatchModeEnd(kBatchTag);
+  HIXL_CHK_BOOL_RET_STATUS(ret == 0, FAILED, "[HixlBatchPutAndGet] HcommBatchModeEnd faild, ret is %d", ret);
+  HIXL_LOGI("[HixlBatchPutAndGet] HcommBatchModeEnd end");
+  return SUCCESS;
+}
+}  // namespace
+
+extern "C" uint32_t HixlBatchPut(HixlOneSideOpParam *param) {
+  uint32_t ret = HixlBatchTransfer(true, param);
+  if (ret != 0) {
+    HIXL_LOGE(FAILED, "[HixlBatchPut] HixlBatchPut faild, ret is %u", ret);
+    return FAILED;
+  }
+  return ret;
+}
+
+extern "C" uint32_t HixlBatchGet(HixlOneSideOpParam *param) {
+  uint32_t ret = HixlBatchTransfer(false, param);
+  if (ret != 0) {
+    HIXL_LOGE(FAILED, "[HixlBatchGet] HixlBatchGet faild, ret is %u", ret);
+    return FAILED;
+  }
+  return ret;
+}
+}  // namespace hixl
---
+++ tests/cpp/ops/hixl_kernel/hixl_kernel_basic_unittest.cc
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+#include <array>
+#include "gtest/gtest.h"
+#include "hixl/hixl_types.h"
+#include "hixl_kernel/hixl_batch_transfer.h"
+
+namespace {
+template <size_t kN>
+HixlOneSideOpParam CreateTestParamFixed(std::array<std::array<uint8_t, 8>, kN> &src_buffers,
+                                        std::array<std::array<uint8_t, 8>, kN> &dst_buffers,
+                                        std::array<uint64_t, kN> &lens_storage, uint64_t remote_flag_addr,
+                                        uint64_t local_flag_addr, ThreadHandle thread = 0ULL,
+                                        ChannelHandle channel = 0ULL) {
+  // 在调用者栈上创建数组，通过引用传递
+  static std::array<void *, kN> src_ptrs;
+  static std::array<void *, kN> dst_ptrs;
+  static std::array<uint64_t, kN> lens;
+
+  for (size_t i = 0; i < kN; ++i) {
+    src_ptrs[i] = src_buffers[i].data();
+    dst_ptrs[i] = dst_buffers[i].data();
+    lens[i] = lens_storage[i];
+  }
+
+  HixlOneSideOpParam param{};
+  param.thread = thread;
+  param.channel = channel;
+  param.list_num = static_cast<uint32_t>(kN);
+  param.dst_buf_addr_list = dst_ptrs.data();
+  param.src_buf_addr_list = src_ptrs.data();
+  param.len_list = lens.data();
+  param.remote_flag_addr = remote_flag_addr;
+  param.local_flag_addr = local_flag_addr;
+  param.flag_size = sizeof(uint64_t);
+
+  return param;
+}
+}  // namespace
+
+using namespace hixl;
+
+TEST(HixlKernelBasicTest, BatchPutSuccess) {
+  // 模拟本地源数据和远端目标缓冲区
+  std::array<std::array<uint8_t, 8>, 3> local_src{};
+  std::array<std::array<uint8_t, 8>, 3> remote_dst{};
+  std::array<uint64_t, 3> lens_storage{8, 8, 8};
+
+  for (auto &arr : local_src) {
+    std::fill(arr.begin(), arr.end(), 0xAA);
+  }
+  for (auto &arr : remote_dst) {
+    std::fill(arr.begin(), arr.end(), 0xBB);
+  }
+  uint64_t remote_flag_addr = 1ULL;
+  uint64_t local_flag_addr = 0ULL;
+  auto param =
+      CreateTestParamFixed<3>(local_src, remote_dst, lens_storage, reinterpret_cast<uint64_t>(&remote_flag_addr),
+                              reinterpret_cast<uint64_t>(&local_flag_addr));
+  uint32_t ret = HixlBatchPut(&param);
+  EXPECT_EQ(ret, SUCCESS);
+}
+
+TEST(HixlKernelBasicTest, BatchGetSuccess) {
+  // 模拟远端源数据和本地目标缓冲区
+  std::array<std::array<uint8_t, 8>, 3> remote_src{};
+  std::array<std::array<uint8_t, 8>, 3> local_dst{};
+  std::array<uint64_t, 3> lens_storage{8, 8, 8};
+
+  for (auto &arr : remote_src) {
+    std::fill(arr.begin(), arr.end(), 0xAA);
+  }
+  for (auto &arr : local_dst) {
+    std::fill(arr.begin(), arr.end(), 0xBB);
+  }
+  uint64_t remote_flag_addr = 1ULL;
+  uint64_t local_flag_addr = 0ULL;
+  auto param =
+      CreateTestParamFixed<3>(remote_src, local_dst, lens_storage, reinterpret_cast<uint64_t>(&remote_flag_addr),
+                              reinterpret_cast<uint64_t>(&local_flag_addr));
+  uint32_t ret = HixlBatchGet(&param);
+  EXPECT_EQ(ret, SUCCESS);
+}
+
+TEST(HixlKernelBasicTest, BatchPutSingleTask) {
+  std::array<std::array<uint8_t, 8>, 1> local_src{};
+  std::array<std::array<uint8_t, 8>, 1> remote_dst{};
+  std::array<uint64_t, 1> lens_storage{0};
+
+  uint64_t remote_flag_addr = 1ULL;
+  uint64_t local_flag_addr = 0ULL;
+  auto param =
+      CreateTestParamFixed<1>(local_src, remote_dst, lens_storage, reinterpret_cast<uint64_t>(&remote_flag_addr),
+                              reinterpret_cast<uint64_t>(&local_flag_addr));
+
+  uint32_t ret = HixlBatchPut(&param);
+  EXPECT_EQ(ret, FAILED);
+}
+
+TEST(HixlKernelBasicTest, BatchGetSingleTask) {
+  std::array<std::array<uint8_t, 8>, 1> remote_src{};
+  std::array<std::array<uint8_t, 8>, 1> local_dst{};
+  std::array<uint64_t, 1> lens_storage{0};
+
+  uint64_t remote_flag_addr = 1ULL;
+  uint64_t local_flag_addr = 0ULL;
+  auto param =
+      CreateTestParamFixed<1>(remote_src, local_dst, lens_storage, reinterpret_cast<uint64_t>(&remote_flag_addr),
+                              reinterpret_cast<uint64_t>(&local_flag_addr));
+
+  uint32_t ret = HixlBatchGet(&param);
+  EXPECT_EQ(ret, FAILED);
+}
