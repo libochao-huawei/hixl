05a81a6adf20e8aaacf136dc790727c1eadb9043
lugang11 <lugang11@huawei.com>
2026-03-06 11:13:19 +0800

    llmdatadist增加日志打印

--- src/llm_datadist/adxl/buffer_transfer_service.cc
+++ src/llm_datadist/adxl/buffer_transfer_service.cc
@@ -640,7 +640,13 @@ Status BufferTransferService::HandleBufferResp(const ChannelPtr &channel, Buffer
     ReleaseBuffer(ptr);
     req_id_buffers_[buffer_resp.req_id].erase(ptr);
   }
-  LLMLOGI("Recv resp, req id:%lu.", buffer_resp.req_id);
+  LLMLOGI("Recv resp, req_id:%lu, timeout:%lu, transfer_type:%d, buffer_addr:%lu, src_addrs.size:%zu, buffer_lens.size:%zu",
+        buffer_resp.req_id,
+        buffer_resp.timeout,
+        static_cast<int>(buffer_resp.transfer_type),
+        buffer_resp.buffer_addr,
+        buffer_resp.src_addrs.size(),
+        buffer_resp.buffer_lens.size());
   return SUCCESS;
 }
 
