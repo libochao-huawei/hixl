9b3c0cf1f9802ac8a8381cd2fa551cb564b189be
youxiao <youxiao@huawei.com>
2026-03-17 09:31:14 +0800

    ReserveMemory make sure init first

--- src/llm_datadist/adxl/virtual_memory_manager.cc
+++ src/llm_datadist/adxl/virtual_memory_manager.cc
@@ -68,6 +68,11 @@ Status VirtualMemoryManager::Initialize() {
   if (initialized_) {
     return SUCCESS;
   }
+  ADXL_CHK_STATUS_RET(InitProcess());
+  return SUCCESS;
+}
+
+Status VirtualMemoryManager::InitProcess() {
   // Use user-set capacity if already set, otherwise use default
   if (vm_size_ == 0) {
     vm_size_ = kDefaultGlobalVirtualMemorySize;
@@ -110,9 +115,7 @@ Status VirtualMemoryManager::ReserveMemory(size_t size, uintptr_t &mem_addr) {
   }
 
   if (!initialized_) {
-    ADXL_CHK_STATUS_RET(ReserveMemAddress(global_virtual_memory_, vm_size_));
-    global_virtual_memory_addr_ = llm::PtrToValue(global_virtual_memory_);
-    initialized_ = true;
+    ADXL_CHK_STATUS_RET(InitProcess());
   }
 
   // Calculate number of 1GB blocks needed (round up)
