4c68b6b5a0a49f90030f03a2f6a6b9a548a752ef
lugang11 <lugang11@huawei.com>
2026-03-17 12:30:15 +0800

    适配HCCL接口改动

--- tests/depends/hccl/src/hccl_stub.cc
+++ tests/depends/hccl/src/hccl_stub.cc
@@ -104,7 +104,8 @@ int32_t HcommChannelFence(ChannelHandle channel) {
   return HCCL_SUCCESS;
 }
 
-int32_t HcommWriteNbi(ChannelHandle channel, void *dst, void *src, uint64_t len) {
+int32_t HcommWriteNbiOnThread(ThreadHandle thread, ChannelHandle channel, void *dst, void *src, uint64_t len) {
+  (void)thread;
   (void)channel;
   if (dst == nullptr || src == nullptr || len == 0) {
     return HCCL_E_PARA;
@@ -115,7 +116,8 @@ int32_t HcommWriteNbi(ChannelHandle channel, void *dst, void *src, uint64_t len)
   return HCCL_SUCCESS;
 }
 
-int32_t HcommReadNbi(ChannelHandle channel, void *dst, void *src, uint64_t len) {
+int32_t HcommReadNbiOnThread(ThreadHandle thread, ChannelHandle channel, void *dst, void *src, uint64_t len) {
+  (void)thread;
   (void)channel;
   if (dst == nullptr || src == nullptr || len == 0) {
     return HCCL_E_PARA;
