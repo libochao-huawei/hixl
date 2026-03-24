f6b74d719c4afab15ff890e295f7bf7c10dfd485
lining23666 <lining.li@huawei.com>
2026-03-24 10:02:49 +0800

    refactor: call hcomm by proxy
    Co-authored-by: lining23666<lining.li@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !302 merge master into master
    
    refactor: call hcomm by proxy
    
    Created-by: lining23666
    Commit-by: lining23666
    Merged-by: cann-robot
    Description: ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [x] 代码重构
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    ## 描述
    由于版本要求hixl仓支持跨版本编译（使用8.5.2版本可以编译最新代码），所以需要拷贝hcomm依赖的头文件，不依赖安装版本提供的头文件。
    计划分几步执行，
    1、把调用的hcomm接口采用单独放到一个proxy中采用弱符号的方式调用
    2、把依赖的hcomm头文件拷贝到hixl仓，其他地方不直接依赖hcomm头文件
    
    当前上库作为第一步，把hcomm接口封装到HcommProxy中调用
    
    
    ## 测试项
    <!--描述进行了哪些测试来验证本次改动，或新增了哪些测试用例。-->
    本次不涉及功能改动，验证已有用例即可。
    
    ## 测试结果
    <!--描述上述测试项的测试结果，可通过表格、图片等形式展示。-->
    example用例执行结果：
    ![image.png](https://raw.gitcode.com/user-images/assets/7838749/d7a2abe0-c766-4646-9005-ad5b1a30ecde/image.png 'image.png')
    涉及ops用例执行结果：
    ![image.png](https://raw.gitcode.com/user-images/assets/7838749/25f27dab-a1b6-43ed-b346-f29245da78cf/image.png 'image.png')
    ![image.png](https://raw.gitcode.com/user-images/assets/7838749/7c77d062-3e99-444e-9087-ae71e1f48e88/image.png 'image.png')
    
    ## Checklist
    <!--  [x] 表示选中 -->
    - [x] 该Pull Request的代码风格和项目的代码风格一致
    - [x] 提交的代码已经过充分的验证
    - [x] 涉及的相关文档修改已更新
    - [x] 标题中正确使用了类型标签（例如：feat/bugfix/refactor/docs/test等）
    
    ## 其它（可选）
    <!--可以在这里描述任何和该Pull Request相关的说明。-->
    
    
    
    See merge request: cann/hixl!302

--- src/hixl/cs/complete_pool.h
+++ src/hixl/cs/complete_pool.h
@@ -18,7 +18,7 @@
 #include "acl/acl.h"
 #include "common/hixl_checker.h"
 #include "cs/hixl_cs.h"
-#include "hcomm_compat.h"
+#include "proxy/hcomm_compat.h"
 #include "runtime/runtime/rt.h"
 
 namespace hixl {
