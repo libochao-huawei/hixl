0842c56a6c103f579293e5e2e7caeeab7864b938
Rentianqi <rentianqi3@huawei.com>
2026-03-24 09:54:25 +0800

    support host api profiling

---
+++ src/hixl/common/prof_api_reg.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
+ * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+ * CANN Open Software License Agreement Version 2.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#ifndef CANN_HIXL_SRC_HIXL_COMMON_PROF_API_REG_H_
+#define CANN_HIXL_SRC_HIXL_COMMON_PROF_API_REG_H_
+
+#include <cstdint>
+#include "acl/acl_base.h"
+#include "aprof_pub.h"
+
+namespace hixl {
+  enum class HixlProfType {
+    // start with 0x10000000U
+    kProfTypeStart = 0x10000000U,
+    HixlProfTypeRead,
+    HixlProfTypeWrite,
+    kProfTypeEnd
+  };
+
+  class HixlProfilingReporter {
+   public:
+    explicit HixlProfilingReporter(const HixlProfType api_id);
+    HixlProfilingReporter(const HixlProfType api_id, uint64_t start_time);
+    virtual ~HixlProfilingReporter() noexcept;
+
+   private:
+    uint64_t start_time_ = 0UL;
+    const HixlProfType hixl_api_;
+  };
+} // namespace hixl
+
+#define HIXL_API_PROFILING(api_id) \
+  const hixl::HixlProfilingReporter profilingReporter(api_id)
+
+#define HIXL_API_PROFILING_WITH_TIME(apiId, startTime) \
+  const hixl::HixlProfilingReporter profilingReporter(apiId, startTime)
+#endif
--- src/hixl/engine/hixl_engine.h
+++ src/hixl/engine/hixl_engine.h
@@ -152,7 +152,7 @@ class HixlEngine : public hixl::Engine {
   HixlServer server_;
   std::map<void *, MemInfo> mem_map_;
   std::vector<EndpointConfig> endpoint_list_;
-  std::map<uint64_t, AscendString> req2client_;
+  std::map<uint64_t, TransferInfo> req_map_;
 };
 }  // namespace hixl
 
