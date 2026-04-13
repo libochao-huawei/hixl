2a63fbc09565fbeace836eeafd0824c19fb8a6aa
y30036845 <yanming41@h-partners.com>
2026-04-13 18:52:27 +0800

    异步建链/断链对接ConnectPoolExecutor

--- src/hixl/common/hixl_utils.cc
+++ src/hixl/common/hixl_utils.cc
@@ -167,17 +167,28 @@ Status CheckOptions(const std::map<AscendString, AscendString> &options) {
   static std::unordered_set<std::string> kOptionsFields = {hixl::OPTION_LOCAL_COMM_RES, hixl::OPTION_BUFFER_POOL, 
                                                            adxl::OPTION_LOCAL_COMM_RES, adxl::OPTION_BUFFER_POOL,
                                                            hixl::OPTION_RDMA_TRAFFIC_CLASS, adxl::OPTION_RDMA_TRAFFIC_CLASS,
-                                                           hixl::OPTION_RDMA_SERVICE_LEVEL, adxl::OPTION_RDMA_SERVICE_LEVEL};
+                                                           hixl::OPTION_RDMA_SERVICE_LEVEL, adxl::OPTION_RDMA_SERVICE_LEVEL,
+                                                           hixl::OPTION_GLOBAL_RESOURCE_CONFIG};
   for (const auto &pair : options) {
     HIXL_CHK_BOOL_RET_SPECIAL_STATUS(kOptionsFields.find(pair.first.GetString()) == kOptionsFields.end(), 
                                      PARAM_INVALID, 
                                      "Invalid option '%s' is not supported, options for hixl engine only support "
-                                     "OPTION_LOCAL_COMM_RES, OPTION_BUFFER_POOL, OPTION_RDMA_TRAFFIC_CLASS and OPTION_RDMA_SERVICE_LEVEL",
+                                     "OPTION_LOCAL_COMM_RES, OPTION_BUFFER_POOL, OPTION_RDMA_TRAFFIC_CLASS, "
+                                     "OPTION_RDMA_SERVICE_LEVEL and OPTION_GLOBAL_RESOURCE_CONFIG",
                                      pair.first.GetString());
     if ((pair.first == hixl::OPTION_BUFFER_POOL) || (pair.first == adxl::OPTION_BUFFER_POOL)) {
       HIXL_CHK_BOOL_RET_STATUS(pair.second.GetString() == std::string("0:0"), 
                                PARAM_INVALID, 
                                "Invalid option fields, OPTION_BUFFER_POOL for hixl engine only supports 0:0");
+    }
+    if (pair.first == hixl::OPTION_GLOBAL_RESOURCE_CONFIG) {
+      try {
+        nlohmann::json j = nlohmann::json::parse(pair.second.GetString());
+        (void)j;
+      } catch (const nlohmann::json::exception &e) {
+        HIXL_LOGE(PARAM_INVALID, "Failed to parse OPTION_GLOBAL_RESOURCE_CONFIG, exception:%s", e.what());
+        return PARAM_INVALID;
+      }
     }
   }
   return SUCCESS;
