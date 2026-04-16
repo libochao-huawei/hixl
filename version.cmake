1e5e743b9faf3067c86cee8ed043d7ae6218449c
xumengxuan <xumengxuan2@huawei.com>
2026-04-16 15:28:05 +0800

    引用工程公共仓

--- version.cmake
+++ version.cmake
@@ -8,12 +8,12 @@
 # See LICENSE in the root of the software repository for the full text of the License.
 # -----------------------------------------------------------------------------------------------------------
 
-set_package(hixl VERSION "9.0.0")
+set_cann_package(hixl VERSION "9.0.0")
 
-set_build_dependencies(runtime ">=9.0.0")
-set_build_dependencies(metadef ">=9.0.0")
-set_build_dependencies(hcomm ">=9.0.0")
+set_cann_build_dependencies(runtime ">=9.0.0")
+set_cann_build_dependencies(metadef ">=9.0.0")
+set_cann_build_dependencies(hcomm ">=9.0.0")
 
-set_run_dependencies(runtime ">=8.5")
-set_run_dependencies(metadef ">=8.5")
-set_run_dependencies(hcomm ">=8.5")
+set_cann_run_dependencies(runtime ">=8.5")
+set_cann_run_dependencies(metadef ">=8.5")
+set_cann_run_dependencies(hcomm ">=8.5")
