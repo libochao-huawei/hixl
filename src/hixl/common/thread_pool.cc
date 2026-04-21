631c82ea79090fce3827235ce2032e1e725de93c
zhaozhihui <zhaozhihui3@yeah.net>
2026-04-21 15:37:46 +0800

    [bugfix]修改benchmark在单进程多client场景的线程setdevice，以及添加skip rpath

--- src/hixl/common/thread_pool.cc
+++ src/hixl/common/thread_pool.cc
@@ -11,13 +11,22 @@
 #include "thread_pool.h"
 
 namespace hixl {
-ThreadPool::ThreadPool(std::string thread_name_prefix, const uint32_t size)
-    : thread_name_prefix_(std::move(thread_name_prefix)), is_stoped_(false) {
-  idle_thrd_num_ = size < 1U ? 1U : size;
+ThreadPool::ThreadPool(std::string thread_name_prefix,
+                       const uint32_t min_size,
+                       const uint32_t max_size)
+    : thread_name_prefix_(std::move(thread_name_prefix)),
+      is_stoped_(false),
+      min_thrd_num_(min_size < 1U ? 1U : min_size),
+      max_thrd_num_(max_size < min_thrd_num_ ? min_thrd_num_ : max_size) {
+  idle_thrd_num_ = min_thrd_num_;
+  total_thrd_num_ = min_thrd_num_;
+  busy_thrd_num_ = 0U;
 
-  for (uint32_t i = 0U; i < idle_thrd_num_; ++i) {
-    pool_.emplace_back(&ThreadFunc, this, i);
+  for (uint32_t i = 0U; i < min_thrd_num_; ++i) {
+    pool_.emplace_back(&ThreadFunc, this, i, false);
   }
+  HIXL_LOGI("[ThreadPool] created, name:%s, min_size:%u, max_size:%u, core_threads:%u",
+            thread_name_prefix_.c_str(), min_thrd_num_, max_thrd_num_, min_thrd_num_);
 }
 
 ThreadPool::~ThreadPool() {
@@ -28,6 +37,8 @@ void ThreadPool::Destroy() {
   if (is_stoped_.load() == true) {
     return;
   }
+  HIXL_LOGI("[ThreadPool] destroying, name:%s, total_threads:%u, pending_tasks:%zu",
+            thread_name_prefix_.c_str(), total_thrd_num_.load(), tasks_.size());
   is_stoped_.store(true);
   {
     const std::unique_lock<std::mutex> lock{m_lock_};
@@ -39,21 +50,41 @@ void ThreadPool::Destroy() {
       try {
         thd.join();
       } catch (...) {
-        HIXL_LOGW("thread join exception");
+        HIXL_LOGW("[ThreadPool:%s] thread join exception", thread_name_prefix_.c_str());
       }
     }
   }
+  HIXL_LOGI("[ThreadPool] destroyed, name:%s", thread_name_prefix_.c_str());
 }
 
-void ThreadPool::ThreadFunc(ThreadPool *const thread_pool, uint32_t thread_idx) {
+void ThreadPool::AddTemporaryThread() {
+  const std::lock_guard<std::mutex> lock{m_lock_};
+  if (total_thrd_num_.load() >= max_thrd_num_) {
+    HIXL_LOGD("[ThreadPool:%s] cannot add temp thread, total:%u >= max:%u",
+              thread_name_prefix_.c_str(), total_thrd_num_.load(), max_thrd_num_);
+    return;
+  }
+  uint32_t thread_idx = total_thrd_num_.load();
+  ++total_thrd_num_;
+  std::thread temp_thread(&ThreadFunc, this, thread_idx, true);
+  temp_thread.detach();
+  HIXL_LOGI("[ThreadPool:%s] add temp thread, idx:%u, idle:%u, busy:%u, total:%u, max:%u",
+            thread_name_prefix_.c_str(), thread_idx,
+            idle_thrd_num_.load(), busy_thrd_num_.load(), total_thrd_num_.load(), max_thrd_num_);
+}
+
+void ThreadPool::ThreadFunc(ThreadPool *const thread_pool, uint32_t thread_idx, bool is_temporary) {
   if (thread_pool == nullptr) {
     return;
   }
+  std::string thread_type = is_temporary ? "temp" : "core";
   if (!thread_pool->thread_name_prefix_.empty()) {
-    auto thread_name = thread_pool->thread_name_prefix_ + std::to_string(thread_idx);
+    auto thread_name = thread_pool->thread_name_prefix_ + "_" + thread_type + "_" + std::to_string(thread_idx);
     int32_t set_ret = pthread_setname_np(pthread_self(), thread_name.c_str());
     HIXL_LOGD("set thread name to [%s], ret=%d", thread_name.c_str(), set_ret);
   }
+  HIXL_LOGD("[ThreadPool:%s] thread started, type:%s, idx:%u",
+             thread_pool->thread_name_prefix_.c_str(), thread_type.c_str(), thread_idx);
 
   while (!thread_pool->is_stoped_) {
     std::function<void()> task;
@@ -64,14 +95,36 @@ void ThreadPool::ThreadFunc(ThreadPool *const thread_pool, uint32_t thread_idx) 
             return thread_pool->is_stoped_.load() || (!thread_pool->tasks_.empty());
           });
       if (thread_pool->is_stoped_ && thread_pool->tasks_.empty()) {
+        if (is_temporary) {
+          --thread_pool->total_thrd_num_;
+          HIXL_LOGI("[ThreadPool:%s] temp thread exit on stop, idx:%u, idle:%u, busy:%u, total:%u",
+                    thread_pool->thread_name_prefix_.c_str(), thread_idx,
+                    thread_pool->idle_thrd_num_.load(), thread_pool->busy_thrd_num_.load(),
+                    thread_pool->total_thrd_num_.load());
+        }
         return;
       }
       task = std::move(thread_pool->tasks_.front());
       thread_pool->tasks_.pop();
     }
-    --thread_pool->idle_thrd_num_;
+    if (!is_temporary) {
+      --thread_pool->idle_thrd_num_;
+    }
+    ++thread_pool->busy_thrd_num_;
     task();
-    ++thread_pool->idle_thrd_num_;
+    --thread_pool->busy_thrd_num_;
+
+    if (is_temporary && !thread_pool->is_stoped_) {
+      --thread_pool->total_thrd_num_;
+      HIXL_LOGI("[ThreadPool:%s] temp thread exit after task, idx:%u, idle:%u, busy:%u, total:%u",
+                thread_pool->thread_name_prefix_.c_str(), thread_idx,
+                thread_pool->idle_thrd_num_.load(), thread_pool->busy_thrd_num_.load(),
+                thread_pool->total_thrd_num_.load());
+      return;
+    }
+    if (!is_temporary) {
+      ++thread_pool->idle_thrd_num_;
+    }
   }
 }
 }  // namespace hixl
