e5e50efe3e5a85e63e4e377047d33726946365bf
Rentianqi <rentianqi3@huawei.com>
2026-04-28 10:20:21 +0800

    decouple llm_datadist v1 and v2

--- src/python/llm_datadist/llm_datadist/data_type.py
+++ src/python/llm_datadist/llm_datadist/data_type.py
@@ -11,7 +11,7 @@
 # ----------------------------------------------------------------------------
 
 from enum import Enum
-from . import metadef_wrapper
+from llm_datadist import metadef_wrapper
 
 
 class DataType(Enum):
