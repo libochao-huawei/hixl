57515fce43b2d07c78612a0bb4afbdb10542d3ad
ZhaoBaiwei <zhaobaiwei@huawei.com>
2026-05-21 08:51:00 +0000

    extract constant

--- src/llm_datadist/adxl/channel_msg_handler.cc
+++ src/llm_datadist/adxl/channel_msg_handler.cc
@@ -29,6 +29,8 @@ namespace adxl {
 namespace {
 constexpr int32_t kWaitRespTime = 20;
 constexpr int32_t kCheckDisconnetPeriod = 10;
+constexpr int32_t kMaxTrafficClassRange = 255;
+constexpr int32_t kMaxServiceLevel = 7;
 }  // namespace
 
 static inline void from_json(const nlohmann::json &j, AddrInfo &op_desc) {
@@ -137,8 +139,7 @@ Status ChannelMsgHandler::ParseTrafficClass(const std::map<AscendString, AscendS
     int32_t traffic_class = 0;
     ADXL_CHK_LLM_RET(llm::LLMUtils::ToNumber(traffic_class_str, traffic_class), "%s is invalid, value = %s",
                      hixl::OPTION_RDMA_TRAFFIC_CLASS, traffic_class_str.c_str());
-    // traffic class must be in [0, 255] and a multiple of 4
-    ADXL_CHK_BOOL_RET_STATUS(traffic_class >= 0 && traffic_class <= 255 && (traffic_class % 4 == 0), PARAM_INVALID,
+    ADXL_CHK_BOOL_RET_STATUS(traffic_class >= 0 && traffic_class <= kMaxTrafficClassRange && (traffic_class % 4 == 0), PARAM_INVALID,
                              "%s is invalid, value = %d, must be between 0-255 and a multiple of 4",
                              hixl::OPTION_RDMA_TRAFFIC_CLASS, traffic_class);
     comm_config_.hcclRdmaTrafficClass = traffic_class;
@@ -163,8 +164,7 @@ Status ChannelMsgHandler::ParseServiceLevel(const std::map<AscendString, AscendS
     int32_t service_level = 0;
     ADXL_CHK_LLM_RET(llm::LLMUtils::ToNumber(service_level_str, service_level), "%s is invalid, value = %s",
                      hixl::OPTION_RDMA_SERVICE_LEVEL, service_level_str.c_str());
-    // service level must be in [0, 7]
-    ADXL_CHK_BOOL_RET_STATUS(service_level >= 0 && service_level <= 7, PARAM_INVALID,
+    ADXL_CHK_BOOL_RET_STATUS(service_level >= 0 && service_level <= kMaxServiceLevel, PARAM_INVALID,
                              "%s must be in [0, 7], value = %d", hixl::OPTION_RDMA_SERVICE_LEVEL, service_level);
     comm_config_.hcclRdmaServiceLevel = service_level;
     LLMLOGI("set rdma service level to %d.", service_level);
