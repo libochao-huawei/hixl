41474744e94908e611ad1e6dfbe65c058cf85fae
ZhaoBaiwei <zhaobaiwei@huawei.com>
2026-05-21 06:43:17 +0000

    fix magic number

--- src/llm_datadist/adxl/channel_msg_handler.cc
+++ src/llm_datadist/adxl/channel_msg_handler.cc
@@ -137,6 +137,7 @@ Status ChannelMsgHandler::ParseTrafficClass(const std::map<AscendString, AscendS
     int32_t traffic_class = 0;
     ADXL_CHK_LLM_RET(llm::LLMUtils::ToNumber(traffic_class_str, traffic_class), "%s is invalid, value = %s",
                      hixl::OPTION_RDMA_TRAFFIC_CLASS, traffic_class_str.c_str());
+    // traffic class must be in [0, 255] and a multiple of 4
     ADXL_CHK_BOOL_RET_STATUS(traffic_class >= 0 && traffic_class <= 255 && (traffic_class % 4 == 0), PARAM_INVALID,
                              "%s is invalid, value = %d, must be between 0-255 and a multiple of 4",
                              hixl::OPTION_RDMA_TRAFFIC_CLASS, traffic_class);
@@ -162,6 +163,7 @@ Status ChannelMsgHandler::ParseServiceLevel(const std::map<AscendString, AscendS
     int32_t service_level = 0;
     ADXL_CHK_LLM_RET(llm::LLMUtils::ToNumber(service_level_str, service_level), "%s is invalid, value = %s",
                      hixl::OPTION_RDMA_SERVICE_LEVEL, service_level_str.c_str());
+    // service level must be in [0, 7]
     ADXL_CHK_BOOL_RET_STATUS(service_level >= 0 && service_level <= 7, PARAM_INVALID,
                              "%s must be in [0, 7], value = %d", hixl::OPTION_RDMA_SERVICE_LEVEL, service_level);
     comm_config_.hcclRdmaServiceLevel = service_level;
