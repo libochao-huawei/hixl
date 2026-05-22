f404317eb5ff97b785c749deb9eca0b91d6d7ca3
andy_git <liaomeifeng@huawei.com>
2026-05-22 17:09:11 +0800

    add qos

--- src/hixl/engine/hixl_options.cc
+++ src/hixl/engine/hixl_options.cc
@@ -75,6 +75,9 @@ void from_json(const nlohmann::json &j, CommResourceConfigDesc &cfg) {
   if (j.contains("comm_resource_config.listen_port")) {
     cfg.listen_port = JsonToNumber<uint32_t>(j.at("comm_resource_config.listen_port"));
   }
+  if (j.contains(kQosName)) {
+    cfg.qos = JsonToNumber<int32_t>(j.at(kQosName));
+  }
 }
 
 void from_json(const nlohmann::json &j, GlobalResourceConfig &cfg) {
@@ -222,15 +225,8 @@ Status HixlOptions::ParseAutoConnectOptions(const std::map<AscendString, AscendS
   return SUCCESS;
 }
 
-Status HixlOptions::ParseGlobalResourceConfig(const std::map<AscendString, AscendString> &options) {
-  const auto &config_it = options.find(hixl::OPTION_GLOBAL_RESOURCE_CONFIG);
-  if (config_it == options.end()) {
-    return SUCCESS;
-  }
-  std::string config_str = config_it->second.GetString();
-  if (config_str.empty()) {
-    return SUCCESS;
-  }
+Status HixlOptions::ParseGlobalResourceConfig(std::string &config_str)
+{
   try {
     auto json = nlohmann::json::parse(config_str);
     if (!json.is_object()) {
@@ -263,13 +259,30 @@ Status HixlOptions::ParseGlobalResourceConfig(const std::map<AscendString, Ascen
                                "comm_resource_config.listen_port must be in [%u, %u], got %u",
                                kMinListenPort, kMaxListenPort, val);
     }
-
+    if (cfg.comm_resource_config.qos.has_value()) {
+      int32_t val = cfg.comm_resource_config.qos.value();
+      HIXL_CHK_BOOL_RET_STATUS(val >= kQosMin && val <= kQosMax, PARAM_INVALID,
+                               "comm_resource_config.qos must be in [%d, %d], got %d",
+                               kQosMin, kQosMax, val);
+    }
     global_resource_config_ = std::move(cfg);
     return SUCCESS;
   } catch (const nlohmann::json::exception &e) {
     HIXL_LOGE(PARAM_INVALID, "Failed to parse GlobalResourceConfig json, exception:%s", e.what());
     return PARAM_INVALID;
   }
+}
+
+Status HixlOptions::ParseGlobalResourceConfig(const std::map<AscendString, AscendString> &options) {
+  const auto &config_it = options.find(hixl::OPTION_GLOBAL_RESOURCE_CONFIG);
+  if (config_it == options.end()) {
+    return SUCCESS;
+  }
+  std::string config_str = config_it->second.GetString();
+  if (config_str.empty()) {
+    return SUCCESS;
+  }
+  return ParseGlobalResourceConfig(config_str);
 }
 
 }  // namespace hixl
